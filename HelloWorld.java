556177218336d2cce7d7e16bc6c3566c927f3ce7
v-anya <anyamariapaula@hotmail.com>
2023-01-30 21:12:24 -0500

    Create HelloWord program

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+   public static void main(String[] args)
+   {
+      double number = 15.448943;
+      double leftover = number % 0.01;
+      double rounded = number - leftover;
+      
+      System.out.println(rounded);
+      }
+}
+      
